845db7a2b7bc87de854e259eab266c25ace67ccf
thanhduy <thanhduy2198@gmail.com>
2021-03-02 17:20:14 +0700

    Initial Commit

---
+++ BookTheCoach/BookTheCoachApp.swift
@@ -0,0 +1,17 @@
+//
+//  BookTheCoachApp.swift
+//  BookTheCoach
+//
+//  Created by Thanh Duy on 02/03/2021.
+//
+
+import SwiftUI
+
+@main
+struct BookTheCoachApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
