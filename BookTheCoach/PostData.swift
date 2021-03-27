6a22c32aa28ee9f7749e31b7dd39a3100d646e44
thanhduy <thanhduy2198@gmail.com>
2021-03-27 15:00:53 +0700

    post dât

---
+++ BookTheCoach/PostData.swift
@@ -0,0 +1,20 @@
+//
+//  PostData.swift
+//  BookTheCoach
+//
+//  Created by Thanh Duy on 27/03/2021.
+//
+
+import SwiftUI
+
+struct PostData: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct PostData_Previews: PreviewProvider {
+    static var previews: some View {
+        PostData()
+    }
+}
